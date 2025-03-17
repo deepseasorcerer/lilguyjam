b96301ab9ed710498b8b2f2a8aad859c79fc04c4
Cathy Escarsega <caescarsega@gmail.com>
2025-03-17 11:58:19 -0700

    MainMenu
    Added play, settings, and quit buttons. Still a work in progress.

---
+++ Assets/Scripts/UI/MainMenuManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+public class MenuManager : MonoBehaviour
+{
+    [System.Serializable]
+    public class Menu
+    {
+        public string menuName;
+        public GameObject menuObject;
+    }
+
+    [SerializeField] private List<Menu> menus;
+    private Menu currentOpenMenu;
+
+    public void OpenMenu(string menuName)
+    {
+        // Close the currently open menu (if any)
+        if (currentOpenMenu != null)
+        {
+            currentOpenMenu.menuObject.SetActive(false);
+        }
+
+        // Find and open the requested menu
+        foreach (var menu in menus)
+        {
+            if (menu.menuName == menuName)
+            {
+                menu.menuObject.SetActive(true);
+                currentOpenMenu = menu;
+                return;
+            }
+        }
+    }
+
+    public void CloseCurrentMenu()
+    {
+        if (currentOpenMenu != null)
+        {
+            currentOpenMenu.menuObject.SetActive(false);
+            currentOpenMenu = null;
+        }
+    }
+}
+
